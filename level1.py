b0b3df9565eda38b28fbdb2d22391b9a6ce2500b
Matti Gilmore <rev.matthewagilmore@gmail.com>
2017-01-29 07:34:07 -0600

    Add files via upload

---
+++ level1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+global number
+number=0
+while (number <=0 or number <=1001):
+
+	
+	numx=input('enter a number 1-1000: \n')
+	'''nexy line should check  if and letter are in input reseach and  test on dif py'''
+	re.search('[a-zA-Z]', num)
+	number=int(num)
+print("pass")
+
+
+
+'''
+a = 123456
+b = str(a)
+c = []
+
+for digit in b:
+    c.append (int(digit))
+
+print c
+
+'''
